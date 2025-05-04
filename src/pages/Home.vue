93cdcea8345d4720c2b1a284633b57f99c3dc52f
檬 柠 <eefen88@163.com>
2025-05-05 02:51:05 +0800

    docs(scaffolding): 添加 Vuetify 项目脚手架配置文件
    - 新增 .browserslistrc、.editorconfig、.eslintrc-auto-import.json 等配置文件
    - 创建 README.md，介绍项目结构和使用说明
    - 添加 env.d.ts 和 eslint.config.js 文件
    - 创建 index.html，作为项目入口

---
+++ src/pages/Home.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p>Welcome to your Vue.js app!</p>
+    <p>To get started, edit <code>src/pages/Home.vue</code> and save to reload.</p>
+  </div>
+</template>
+
+<style lang="sass">
+</style>
